0b70fc96ff29eced187fd5d7c4fe286a98f10ea2
skdaccess <skdaccess@mit.edu>
2016-05-18 10:34:13 -0400

    Rearranged repository to duplicate the code structure necessary for building pypi package.

---
+++ setup.py
@@ -0,0 +1,33 @@
+# from distutils.core import setup
+from setuptools import setup
+from setuptools import find_packages
+
+package_name = 'skdaccess'
+
+package_list = find_packages()
+
+setup(name     = package_name,
+      version  = '0.9.1',
+      packages = package_list,
+      zip_safe = False,
+      scripts=['bin/skdaccess'],      
+      install_requires = ['tqdm',
+                          'wget',
+                          'numpy>=1.10',
+                          'pandas>=0.17',
+                          'tables',
+                          'scipy',
+                          'setuptools',
+                          'astropy'],
+      description = 'Sci-kit Data Access Package for accessing scientific data sets.',
+      author = 'MITHAGI',
+      author_email='skdaccess@mit.edu',
+      classifiers=[
+          'Topic :: Scientific/Engineering',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3 :: Only'
+          ],
+
+      package_data={'skdaccess': ['examples/groundwater_example.py','docs/skdaccess_Doxygen.pdf','docs/skdaccess_manual.pdf']}
+      )
