80256f5c48b97fdb0d4e03532c4faf64a8a92996
Cody Rude <cmrude@mit.edu>
2017-11-01 16:22:48 -0400

    Updated timeout

--- run_tests.py
+++ run_tests.py
@@ -2,18 +2,18 @@
 
 # The MIT License (MIT)
 # Copyright (c) 2017 Massachusetts Institute of Technology
-# 
+#
 # Author: Cody Rude
 # This software has been created in projects supported by the US National
 # Science Foundation and NASA (PI: Pankratius)
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
@@ -47,7 +47,7 @@ for test in test_list:
     with TemporaryDirectory() as tmpdir:
         try:
             print("Running:", test, end=' ', flush=True)
-            res = check_call([jupyter,'nbconvert','--output=' + os.path.join(tmpdir, 'test_output'), '--ExecutePreprocessor.timeout=180',
+            res = check_call([jupyter,'nbconvert','--output=' + os.path.join(tmpdir, 'test_output'), '--ExecutePreprocessor.timeout=1200',
                               '--ExecutePreprocessor.enabled=True', test],
                              stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
 
@@ -65,5 +65,3 @@ else:
         print("There was 1 failure")
     else:
         print("There were", num_fails, "failures")
-
-
