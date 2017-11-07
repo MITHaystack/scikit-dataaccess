c6efe559212913bce62eac0126f3fe8a42cf44f7
Cody Rude <cmrude@mit.edu>
2017-11-07 15:34:22 -0500

    Increase version number

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.rst") as rfile:
 
 
 setup(name     = package_name,
-      version  = '1.9.5post1',
+      version  = '1.9.6',
       packages = package_list,
       zip_safe = False,
 
